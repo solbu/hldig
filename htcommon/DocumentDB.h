b9be4a4e244e1e96e9a45486a91887d51b9e33cd
turtle <turtle>
1998-01-05 00:47:27 +0000

    reformatting

--- htcommon/DocumentDB.h
+++ htcommon/DocumentDB.h
@@ -2,14 +2,18 @@
 // DocumentDB.h
 //
 // This class is the interface to the database of document references.
-// This database is only used while digging.  An extract of this database is used
-// for searching.  This is because digging requires a different index than searching.
+// This database is only used while digging.  An extract of this
+// database is used for searching.  This is because digging requires a
+// different index than searching.
 //
-// $Id: DocumentDB.h,v 1.1 1997/02/03 17:11:07 turtle Exp $
+// $Id: DocumentDB.h,v 1.2 1998/01/05 00:47:27 turtle Exp $
 //
 // $Log: DocumentDB.h,v $
-// Revision 1.1  1997/02/03 17:11:07  turtle
-// Initial revision
+// Revision 1.2  1998/01/05 00:47:27  turtle
+// reformatting
+//
+// Revision 1.1.1.1  1997/02/03 17:11:07  turtle
+// Initial CVS
 //
 //
 #ifndef _DocumentDB_h_
@@ -23,45 +27,45 @@
 class DocumentDB
 {
 public:
-	//
-	// Construction/Destruction
-	//
-					DocumentDB();
-					~DocumentDB();
+    //
+    // Construction/Destruction
+    //
+    DocumentDB();
+    ~DocumentDB();
 
-	//
-	// The database used for searching is generated from our internal database:
-	//
-	int				CreateSearchDB(char *filename);
+    //
+    // The database used for searching is generated from our internal database:
+    //
+    int			CreateSearchDB(char *filename);
 
-	//
-	// Standard database operations
-	//
-	int				Open(char *filename);
-	int				Read(char *filename);
-	int				Close();
+    //
+    // Standard database operations
+    //
+    int			Open(char *filename);
+    int			Read(char *filename);
+    int			Close();
 
-	int				Add(DocumentRef &);
-	DocumentRef		*operator [] (char *url);
-	int				Exists(char *url);
-	int				Delete(char *url);
+    int			Add(DocumentRef &);
+    DocumentRef		*operator [] (char *url);
+    int			Exists(char *url);
+    int			Delete(char *url);
 
-	//
-	// The database keeps track of document ids.  Here is a way to get the next
-	// document id.
-	//
-	int				NextDocID()					{return nextDocID++;}
+    //
+    // The database keeps track of document ids.  Here is a way to get
+    // the next document id.
+    //
+    int			NextDocID()		{return nextDocID++;}
 
-	//
-	// We will need to be able to iterate over the complete database.
-	//
-	List			*URLs();		// This returns a list of all the URLs
+    //
+    // We will need to be able to iterate over the complete database.
+    //
+    List		*URLs();	// This returns a list of all the URLs
 
 private:
-	Database		*dbf;
-	int				isopen;
-	int				isread;
-	int				nextDocID;
+    Database		*dbf;
+    int			isopen;
+    int			isread;
+    int			nextDocID;
 };
 
 #endif
