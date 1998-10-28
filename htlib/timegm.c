4a98d1f0f2e54e763fe4fbbf20016145568396e6
ghutchis <ghutchis>
1998-10-28 01:04:25 +0000

    Fixes for timegm replacement.

--- htlib/timegm.c
+++ htlib/timegm.c
@@ -37,7 +37,7 @@
 #endif
 
 #ifndef __gmtime_r
-extern struct tm *__gmtime_r __P ((__const time_t *__timer, \
+extern struct tm *__gmtime_r __P ((const time_t *__timer, \
                struct tm *__tp));
 #endif
 
