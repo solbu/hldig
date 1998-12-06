41d8fde7ff880da16caf3c5dc94df14a68460155
ghutchis <ghutchis>
1998-12-06 18:46:22 +0000

    Ensure blank WordRecords have a default count of 1 since a word has to exist to have a WordRecord!

--- htcommon/WordRecord.h
+++ htcommon/WordRecord.h
@@ -1,11 +1,15 @@
 	//
 // WordRecord.h
 //
-// $Id: WordRecord.h,v 1.1 1997/02/03 17:11:07 turtle Exp $
+// $Id: WordRecord.h,v 1.2 1998/12/06 18:46:22 ghutchis Exp $
 //
 // $Log: WordRecord.h,v $
-// Revision 1.1  1997/02/03 17:11:07  turtle
-// Initial revision
+// Revision 1.2  1998/12/06 18:46:22  ghutchis
+// Ensure blank WordRecords have a default count of 1 since a word has to exist
+// to have a WordRecord!
+//
+// Revision 1.1.1.1  1997/02/03 17:11:07  turtle
+// Initial CVS
 //
 //
 #ifndef _WordRecord_h_
@@ -21,7 +25,8 @@ struct WordRecord
 
 	void	Clear()
 			{
-				count = id = weight = anchor = location = 0;
+				id = weight = anchor = location = 0;
+				count = 1;
 			}
 };
 
