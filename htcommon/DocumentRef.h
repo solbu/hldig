63b6b8f57ce78023cf4b83ad0a04bd5446c7feba
ghutchis <ghutchis>
1999-01-05 19:29:40 +0000

    Added comments on the members (fields) of DocumentRef objects.

--- htcommon/DocumentRef.h
+++ htcommon/DocumentRef.h
@@ -1,19 +1,19 @@
 //
 // DocumentRef.h
 //
-// $Id: DocumentRef.h,v 1.8 1998/11/15 22:29:27 ghutchis Exp $
+// $Id: DocumentRef.h,v 1.9 1999/01/05 19:29:40 ghutchis Exp $
 //
 // $Log: DocumentRef.h,v $
-// Revision 1.8  1998/11/15 22:29:27  ghutchis
+// Revision 1.9  1999/01/05 19:29:40  ghutchis
+// Added comments on the members (fields) of DocumentRef objects.
 //
+// Revision 1.8  1998/11/15 22:29:27  ghutchis
 // Implement docBackLinks backlink count.
 //
 // Revision 1.7  1998/09/10 04:16:25  ghutchis
-//
 // More bug fixes.
 //
 // Revision 1.6  1998/09/07 04:37:16  ghutchis
-//
 // Added DocState for documents marked as "noindex".
 //
 // Revision 1.5  1998/08/11 08:58:25  ghutchis
@@ -122,34 +122,62 @@ class DocumentRef : public Object
     //
     // These values will be stored when serializing
     //
+
+    // This is the index number of the document in the database.
     int			docID;
+    // This is the URL of the document.
     String		docURL;
+    // This is the time specified in the document's header
+    // Usually that's the last modified time, for servers that return it.
     time_t		docTime;
+    // This is the time that the last retrieval occurred.
     time_t		docAccessed;
+    // This is the stored excerpt of the document, just text.
     String		docHead;
+    // This is the document-specified description.
+    // For HTML, that's the META description tag.
     String              docMetaDsc;
+    // This is the title of the document.
     String		docTitle;
+    // This is a list of Strings, the text of links pointing to this document.
+    // (e.g. <a href="docURL">description</a>
     List		descriptions;
+    // This is the state of the document--modified, normal, etc.
     ReferenceState	docState;
+    // This is the size of the original document.
     int			docSize;
+    // This is a count of the links in the document (outgoing links).
     int			docLinks;
+    // This is a count of the links to the document (incoming links).
     int                 docBackLinks;
+    // This is the size of the document when including images.
     int			docImageSize;
+    // This is a list of the anchors in the document (i.e. <A NAME=...)
     List		docAnchors;
+    // This is a count of the number of hops from start_urls to here.
     int			docHopCount;
+    // This is a signature of the document. (e.g. md5sum, checksum...)
+    // This is currently unused.
     long int                 docSig;
 	
     //
     // The following values are for the email notification of expiration
     //
+
+    // This is the email destination for htnotify.
     String		docEmail;
+    // This is the date that htnotify should use as comparison.
     String		docNotification;
+    // This is the subject of the email sent out by htnotify.
     String		docSubject;
 
     //
     // This is used for searching and is not stored in the database
     //
+    
+    // This is the current score of this document.
     int			docScore;
+    // This is the nearest anchor for the search word.
     int			docAnchor;
 };
 
