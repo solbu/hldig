232c81694f1f5dfc94db18179096308024da7fb4
bergolth <bergolth>
1999-01-26 17:16:10 +0000

    minor changes

--- htdig/Retriever.h
+++ htdig/Retriever.h
@@ -1,46 +1,9 @@
 //
 // Retriever.h
 //
-// $Id: Retriever.h,v 1.7 1999/01/17 20:32:21 ghutchis Exp $
+// $Id: Retriever.h,v 1.8 1999/01/26 17:16:11 bergolth Exp $
 //
-// $Log: Retriever.h,v $
-// Revision 1.7  1999/01/17 20:32:21  ghutchis
-// Added support for url_log, save and restart digs.
-//
-// Revision 1.6  1998/12/05 00:52:55  ghutchis
-//
-// Added a parameter to Initial function to prevent URLs from being checked
-// twice during an update dig.
-//
-// Revision 1.5  1998/11/27 18:34:52  ghutchis
-//
-// Changed Retriever::got_word to check for small words, valid_punctuation to
-// remove bugs in HTML.cc.
-//
-// Revision 1.4  1998/09/07 04:37:16  ghutchis
-//
-// Added DocState for documents marked as "noindex".
-//
-// Revision 1.3  1998/08/11 08:58:32  ghutchis
-// Second patch for META description tags. New field in DocDB for the
-// desc., space in word DB w/ proper factor.
-//
-// Revision 1.2  1998/07/09 09:39:00  ghutchis
-//
-//
-// Added support for local file digging using patches by Pasi. Patches
-// include support for local user (~username) digging.
-//
-// Revision 1.1.1.1  1997/02/03 17:11:06  turtle
-// Initial CVS
-//
-// Revision 1.1  1995/12/11 22:47:02  turtle
-// This uses the backwards model of only parsing HTML
-//
-// Revision 1.0  1995/08/18 16:27:04  turtle
-// Before change to use of Server class
-//
-//
+
 #ifndef _Retriever_h_
 #define _Retriever_h_
 
@@ -57,7 +20,7 @@ class URLRef;
 enum  RetrieverLog {
     Retriever_noLog,
     Retriever_logUrl,
-    Retriever_Restart,
+    Retriever_Restart
 };
 
 class Retriever
