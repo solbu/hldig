eddb5836ca6a2d54de819839a5a632bb9d660bfb
ghutchis <ghutchis>
1999-01-26 00:42:15 +0000

    Make error message for missing entries conditional to DEBUG symbol. Removes odd error messages under normal use.

--- htlib/Configuration.cc
+++ htlib/Configuration.cc
@@ -2,39 +2,11 @@
 // Configuration.cc
 //
 // Implementation of the Configuration class
-// fix: Set time format to standard to avoid sending If-Modified-Since
-// http headers in native format. 
-//
-// $Log: Configuration.cc,v $
-// Revision 1.8  1999/01/25 04:09:54  ghutchis
-// Complain when an option to be retrieved is not in the list.
-//
-// Revision 1.7  1999/01/23 02:23:55  ghutchis
-// Add support for keyword "include" to include other config files.
-//
-// Revision 1.6  1999/01/14 00:26:48  ghutchis
-// Fixed time format to standard to avoid sending If-Modified-Since http
-// headers in native format (which would be incorrect behavior). Use C locale.
-// 
-// Revision 1.5  1999/01/02 16:13:30  bergolth
-// added warning message if locale selection failed
-//
-// Revision 1.4  1998/08/04 15:36:43  ghutchis
-// Added fix by Philippe Rochat <prochat@lbdsun.epfl.ch> to remove
-// whitespace after config options.
-//
-// Revision 1.3  1998/06/22 04:33:19  turtle
-// New Berkeley database stuff
-//
-// Revision 1.2  1997/03/24 04:33:19  turtle
-// Renamed the String.h file to htString.h to help compiling under win32
-//
-// Revision 1.1.1.1  1997/02/03 17:11:04  turtle
-// Initial CVS
+// Standard class for storing all user options (and defaults)
 //
 //
 #if RELEASE
-static char	RCSid[] = "$Id: Configuration.cc,v 1.8 1999/01/25 04:09:54 ghutchis Exp $";
+static char	RCSid[] = "$Id: Configuration.cc,v 1.9 1999/01/26 00:42:16 ghutchis Exp $";
 #endif
 
 #include "Configuration.h"
@@ -214,7 +186,9 @@ char *Configuration::Find(char *name)
     }
     else
     {
+#ifdef DEBUG
         cerr << "Could not find configuration option " << name << "\n";
+#endif
         return 0;
     }
 }
