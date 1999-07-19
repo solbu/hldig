056e435a894df452f075a508523da9517398e5c5
loic <loic>
1999-07-19 09:28:08 +0000

    Makefiles for automake and utility scripts and files

---
+++ acconfig.h
@@ -0,0 +1,44 @@
+/* Defined in case the compiler doesn't have TRUE and FALSE constants */
+#ifndef TRUE
+#define TRUE 1
+#endif
+
+#ifndef FALSE
+#define FALSE 0
+#endif
+
+#define	VERSION	1
+#define	MAX_WORD_LENGTH	12
+
+/* Define to the syslog level for htsearch logging. */
+#define LOG_LEVEL LOG_INFO
+
+/* Define to the syslog facility for htsearch logging. */
+#define LOG_FACILITY LOG_LOCAL5
+
+@TOP@
+
+/* Define this to the type of the third argument of getpeername() */
+#undef GETPEERNAME_LENGTH_T
+
+/* Define if you have the bool type */
+#undef HAVE_BOOL
+
+#ifndef HAVE_BOOL
+typedef char bool;
+#endif
+
+/* Define if you C++ compiler doesn't know true and false */
+#undef HAVE_TRUE
+#undef HAVE_FALSE
+
+#ifndef HAVE_TRUE
+#define true  TRUE
+#endif
+#ifndef HAVE_FALSE
+#define false FALSE
+#endif
+
+/* Define if you need a prototype for gethostname() */
+#undef NEED_PROTO_GETHOSTNAME
+
