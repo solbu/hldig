dde09d22dcab075f025efe4efcaeab6231c2c578
ghutchis <ghutchis>
1999-07-19 01:50:25 +0000

    Add database access routines to match DocumentDB.cc. (Word): Recognize flags instead of weight, simply add the word. (Duplicates expected!) (mark*): Simply delete the list of words. (flush): Rather than dump to a text file, dump directly to the db.

--- htcommon/WordList.cc
+++ htcommon/WordList.cc
@@ -1,22 +1,33 @@
 //
 // WordList.cc
 //
-// Implementation of WordList
-// Keeps track of the words in each document, spilling out to disk
-// when the document is fully parsed
+//: Interface to the word database. Previously, this wrote to a temporary text
+//  file. Now it writes directly to the word database. 
+//  NOTE: Some code previously attempted to directly read from the word db.
+//  This will no longer work, so it's preferred to use the access methods here
+//
+// Part of the ht://Dig package   <http://www.htdig.org/>
+// Copyright (c) 1999 The ht://Dig Group
+// For copyright details, see the file COPYING in your distribution
+// or the GNU Public License version 2 or later
+// <http://www.gnu.org/copyleft/gpl.html>
 //
 //
 #if RELEASE
-static char RCSid[] = "$Id: WordList.cc,v 1.18 1999/04/25 01:40:32 ghutchis Exp $";
+static char RCSid[] = "$Id: WordList.cc,v 1.19 1999/07/19 01:50:25 ghutchis Exp $";
 #endif
 
 #include "WordList.h"
 #include "WordReference.h"
+#include "WordRecord.h"
+#include "HtWordType.h"
 #include "Configuration.h"
 #include "htString.h"
+#include "DB2_db.h"
+#include "HtPack.h"
 #include <stdio.h>
 #include <ctype.h>
-#include "HtWordType.h"
+#include <fstream.h>
 
 extern Configuration	config;
 
@@ -25,7 +36,11 @@ extern Configuration	config;
 //
 WordList::WordList()
 {
-    words = new Dictionary(203, 10.0);
+    words = new Dictionary(10271, 10.0);
+
+    // The database itself hasn't been opened yet
+    isopen = 0;
+    isread = 0;
 }
 
 
@@ -35,54 +50,45 @@ WordList::WordList()
 WordList::~WordList()
 {
     delete words;
+
+    if (isopen)
+      Close();
 }
 
 
 //*****************************************************************************
-// void WordList::Word(char *word, int location, int anchor_number, double weight_factor)
+// void WordList::Word
+// (String word, unsigned int location, unsigned int anchor_number,
+//  unsigned long int flags)
 //
-void WordList::Word(char *word, int location, int anchor_number, double weight_factor)
+void WordList::Word (String word, unsigned int location, 
+		     unsigned int anchor_number,
+		     unsigned long int flags)
 {
-  if (weight_factor == 0.0) // Why should we add words with no weight?
+    // Why should we add empty words?
+    if (word.length() == 0)
       return;
-    String		shortword = word;
 
-    shortword.lowercase();
-    word = shortword.get();
-    if (shortword.length() > MAX_WORD_LENGTH)
-	word[MAX_WORD_LENGTH] = '\0';
+    // Let's start to clean it up
+    String 	orig = word;
+    word.lowercase();
+    
+    if (word != orig)
+      flags |= FLAG_CAPITAL;
 
     if (!valid_word(word))
 	return;
 
-    WordReference	*wordRef = (WordReference *) words->Find(word);
-
-    if (wordRef)
-    {
-	//
-	// Already had this word.  Update the record
-	//
-	wordRef->WordCount++;
-	wordRef->Weight += int((1000 - location) * weight_factor);
-	if (location < wordRef->Location)
-	  wordRef->Location = location;
-	if (anchor_number < wordRef->Anchor)
-	  wordRef->Anchor = anchor_number;
-    }
-    else
-    {
-	//
-	// New word.  Create a new reference for it
-	//
-	wordRef = new WordReference;
-	wordRef->WordCount = 1;
-	wordRef->Location = location;
-	wordRef->DocumentID = docID;
-	wordRef->Weight = int((1000 - location) * weight_factor);
-	wordRef->Anchor = anchor_number;
-	strcpy(wordRef->Word, word);
-	words->Add(word, wordRef);
-    }
+    //
+    // New word.  Create a new reference for it
+    //
+    WordReference	*wordRef = new WordReference;
+    wordRef->Location = location;
+    wordRef->DocumentID = docID;
+    wordRef->Flags = flags;
+    wordRef->Anchor = anchor_number;
+    wordRef->Word = word;
+    words->Add(word, wordRef);
 }
 
 
@@ -110,14 +116,13 @@ int WordList::valid_word(char *word)
       if (HtIsStrictWordChar((unsigned char)*word) && !isdigit(*word))
 	{
 	    alpha = 1;
-	    // break;	/* Can't stop here, there may still be control chars! */
+	    // break;  /* Can't stop here, there may still be control chars! */
 	}
       else if (allow_numbers && isdigit(*word))
 	{
 	  alpha = 1;
-	  // break;	/* Can't stop here, there may still be control chars! */
+	  // break;    /* Can't stop here, there may still be control chars! */
 	}
-//    if (*word >= 0 && *word < ' ')
       else if (iscntrl(*word))
 	{
 	    control = 1;
@@ -132,36 +137,54 @@ int WordList::valid_word(char *word)
 
 //*****************************************************************************
 // void WordList::Flush()
-//   Dump the current list of words to the temporary word file.  After
+//   Dump the current list of words to the database.  After
 //   the words have been dumped, the list will be destroyed to make
 //   room for the words of the next document.
 //   
 void WordList::Flush()
 {
-    FILE		*fl = fopen(tempfile, "a");
     WordReference	*wordRef;
+    WordRecord		*wordRec = new WordRecord;
+    Database		*dbf = Database::getDatabaseInstance(DB_BTREE);
+    char		*wordfile = config["word_db"];
+    String		compressedData;
+    String		key;
+
+
+    if (dbf->OpenReadWrite(wordfile, 0664) == NOTOK)
+    {
+      // Need a useful error here, yet we're basically dead.
+    }
 
     words->Start_Get();
     while ((wordRef = (WordReference *) words->Get_NextElement()))
     {
+      if (wordRef->Word.length() == 0)
+	continue;
 
-	fprintf(fl, "%s",wordRef->Word);
-        fprintf(fl, "\ti:%d\tl:%d\tw:%d",
-		wordRef->DocumentID,
-	        wordRef->Location,
-		wordRef->Weight);
-        if (wordRef->WordCount != 1)
-        {
-           fprintf(fl, "\tc:%d",wordRef->WordCount);
-        }
-	if (wordRef->Anchor != 0)
-        {
-           fprintf(fl, "\ta:%d",wordRef->Anchor);
-        }
-        putc('\n', fl);
+      // Construct the key, which is word conjoined by the DocID
+      // \001 is used to join them simply because it's never a word char
+      key = wordRef->Word;
+      key << "\001" << wordRef->DocumentID;
+
+      // Now split out the WordRecord to store
+      wordRec->id = wordRef->DocumentID;
+      wordRec->flags = wordRef->Flags;
+      wordRec->anchor = wordRef->Anchor;
+      wordRec->location = wordRef->Location;
+
+      // We need to compress the WordRecord and convert it into a binary form
+      compressedData = htPack(WORD_RECORD_COMPRESSED_FORMAT,
+			      (char *) wordRec);
+
+      dbf->Put(wordRef->Word, compressedData.get(), compressedData.length());
     }
+
+    // Cleanup
+    delete wordRec;
     words->Destroy();
-    fclose(fl);
+    dbf->Close();
+    delete dbf;
 }
 
 
@@ -171,11 +194,7 @@ void WordList::Flush()
 //
 void WordList::MarkScanned()
 {
-    FILE			*fl = fopen(tempfile, "a");
-
-    fprintf(fl, "+%d\n", docID);
     words->Destroy();
-    fclose(fl);
 }
 
 
@@ -186,11 +205,7 @@ void WordList::MarkScanned()
 //
 void WordList::MarkGone()
 {
-    FILE			*fl = fopen(tempfile, "a");
-
-    fprintf(fl, "-%d\n", docID);
     words->Destroy();
-    fclose(fl);
 }
 
 
@@ -201,11 +216,7 @@ void WordList::MarkGone()
 //
 void WordList::MarkModified()
 {
-    FILE			*fl = fopen(tempfile, "a");
-
-    fprintf(fl, "!%d\n", docID);
     words->Destroy();
-    fclose(fl);
 }
 
 
@@ -222,21 +233,254 @@ void WordList::BadWordFile(char *filename)
     String      new_word;
     int	        minimum_word_length = config.Value("minimum_word_length", 3);
 
+    // Read in the badwords file (it's just a text file)
     while (fl && fgets(buffer, sizeof(buffer), fl))
     {
 	word = strtok(buffer, "\r\n \t");
 	if (word && *word)
 	  {
-	    if (strlen(word) > MAX_WORD_LENGTH)
-	      word[MAX_WORD_LENGTH] = '\0';
-	    new_word = word;  // We need to clean it up before we add it
-	    new_word.lowercase();  // Just in case someone enters an odd one
+	    // We need to clean it up before we add it
+	    // Just in case someone enters an odd one
+	    new_word = word;
+	    new_word.lowercase();
 	    HtStripPunctuation(new_word);
 	    if (new_word.length() >= minimum_word_length)
 	      badwords.Add(new_word, 0);
 	  }
     }
+
     if (fl)
 	fclose(fl);
 }
 
+
+//*****************************************************************************
+// int WordList::Open(char *filename)
+//
+int WordList::Open(char *filename)
+{
+    dbf = 0;
+
+    dbf = Database::getDatabaseInstance(DB_BTREE);
+
+    if (dbf->OpenReadWrite(filename, 0664) != OK)
+        return NOTOK;
+    else
+      {
+	isopen = 1;
+        return OK;
+      }
+}
+
+
+//*****************************************************************************
+// int WordList::Read(char *filename)
+//
+int WordList::Read(char *filename)
+{
+    dbf = 0;
+
+    dbf = Database::getDatabaseInstance(DB_HASH);
+
+    if (dbf->OpenRead(filename) != OK)
+      return NOTOK;
+    else
+      {
+	isopen = 1;
+	isread = 1;
+	return OK;
+      }
+}
+
+
+//*****************************************************************************
+// int WordList::Close()
+//
+int WordList::Close()
+{
+    dbf->Close();
+    delete dbf;
+    dbf = 0;
+    isopen = 0;
+    isread = 0;
+    return OK;
+}
+
+
+//*****************************************************************************
+// int WordList::Add(WordReference *wordRef)
+//
+int WordList::Add(WordReference *wordRef)
+{
+  String	key;
+  String	compressedData;
+  WordRecord	*wordRec = new WordRecord;
+
+  // Construct the key, which is word conjoined by the DocID
+  // \001 is used to join them simply because it's never a word char
+  key = wordRef->Word;
+  key << "\001" << wordRef->DocumentID;
+  
+  // Now split out the WordRecord to store
+  wordRec->id = wordRef->DocumentID;
+  wordRec->flags = wordRef->Flags;
+  wordRec->anchor = wordRef->Anchor;
+  wordRec->location = wordRef->Location;
+  
+  // We need to compress the WordRecord and convert it into a binary form
+  compressedData = htPack(WORD_RECORD_COMPRESSED_FORMAT,
+			  (char *) wordRec);
+  
+  dbf->Put(wordRef->Word, compressedData.get(), compressedData.length());
+
+  delete wordRec;
+
+  return OK;
+}
+
+
+//*****************************************************************************
+// List *WordList::operator [] (String word)
+//
+List *WordList::operator [] (String word)
+{
+    List        *list = new List;
+    char        *key;
+    char        *wordKey;
+    char        *data;
+    String      decompressed;
+    WordRecord  *wr = new WordRecord;
+
+    dbf->Start_Seq(word.get());
+    while ((key = dbf->Get_Next()))
+      {
+        // Break off the \001 and the docID
+        wordKey = strtok(key, "\001");
+        if (word != wordKey)
+            break;
+
+        data = dbf->Get_Item();
+        if (data)
+          {
+            decompressed = htUnpack(WORD_RECORD_COMPRESSED_FORMAT, data);
+            if (decompressed.length() != sizeof (WordRecord))
+              continue;
+
+            memcpy((char *) wr, decompressed.get(), sizeof(WordRecord));
+
+            WordReference *wordRef = new WordReference;
+            wordRef->Word = word;
+            wordRef->DocumentID = wr->id;
+            wordRef->Flags = wr->flags;
+            wordRef->Anchor = wr->anchor;
+            wordRef->Location = wr->location;
+
+            list->Add(wordRef);
+          }
+      }
+
+    delete wr;
+    return list;
+}
+
+
+//*****************************************************************************
+// int WordList::Exists(WordReference wordRef)
+//
+int WordList::Exists(WordReference wordRef)
+{
+    String	key;
+
+    // Construct the key, which is word conjoined by the DocID
+    // \001 is used to join them simply because it's never a word char
+    key = wordRef.Word;
+    key << "\001" << wordRef.DocumentID;
+
+    return dbf->Exists(key);
+}
+
+
+//*****************************************************************************
+// int WordList::Delete(WordReference wordRef)
+//
+int WordList::Delete(WordReference wordRef)
+{
+    String      key;
+
+    // Construct the key, which is word conjoined by the DocID
+    // \001 is used to join them simply because it's never a word char
+    key = wordRef.Word;
+    key << "\001" << wordRef.DocumentID;
+
+    return dbf->Delete(key);
+}
+
+
+//*****************************************************************************
+// List *WordList::Words()
+//
+List *WordList::Words()
+{
+    List	*list = new List;
+    char	*key;
+    String	word;
+    String	lastWord = 0;
+
+    dbf->Start_Get();
+    while ((key = dbf->Get_Next()))
+      {
+	// Break off the \001 and the docID
+	word = strtok(key, "\001");
+
+	if ( (lastWord == 0) || (word != lastWord) )
+	  {
+            list->Add(new String(word));
+	    lastWord = word;
+	  }
+      }
+    return list;
+}
+
+
+//*****************************************************************************
+// List *WordList::WordRefs()
+//
+List *WordList::WordRefs()
+{
+    List        *list = new List;
+    char        *key;
+    char	*word;
+    char	*data;
+    String	decompressed;
+    WordRecord  *wr = new WordRecord;
+
+    dbf->Start_Get();
+    while ((key = dbf->Get_Next()))
+      {
+        // Break off the \001 and the docID
+        word = strtok(key, "\001");
+	cout << " word " << word << endl;
+
+	data = dbf->Get_Item();
+	if (data)
+	  {
+	    decompressed = htUnpack(WORD_RECORD_COMPRESSED_FORMAT, data);
+	    if (decompressed.length() != sizeof (WordRecord))
+	      continue;
+
+	    memcpy((char *) wr, decompressed.get(), sizeof(WordRecord));
+
+	    WordReference *wordRef = new WordReference;
+	    wordRef->Word = word;
+	    wordRef->DocumentID = wr->id;
+	    wordRef->Flags = wr->flags;
+	    wordRef->Anchor = wr->anchor;
+	    wordRef->Location = wr->location;
+	    
+	    list->Add(wordRef);
+	  }
+      }
+
+    delete wr;
+    return list;
+}
--- htcommon/WordList.h
+++ htcommon/WordList.h
@@ -1,22 +1,28 @@
 //
 // WordList.h
 //
-// $Id: WordList.h,v 1.2 1997/03/24 04:33:15 turtle Exp $
+//: Interface to the word database. Previously, this wrote to a temporary text
+//  file. Now it writes directly to the word database.
+//  NOTE: Some code previously attempted to directly read from the word db.
+//  This will no longer work, so it's preferred to use the access methods here
 //
-// $Log: WordList.h,v $
-// Revision 1.2  1997/03/24 04:33:15  turtle
-// Renamed the String.h file to htString.h to help compiling under win32
+// Part of the ht://Dig package   <http://www.htdig.org/>
+// Copyright (c) 1999 The ht://Dig Group
+// For copyright details, see the file COPYING in your distribution
+// or the GNU Public License version 2 or later
+// <http://www.gnu.org/copyleft/gpl.html>
 //
-// Revision 1.1.1.1  1997/02/03 17:11:07  turtle
-// Initial CVS
+// $Id: WordList.h,v 1.3 1999/07/19 01:50:25 ghutchis Exp $
 //
 //
 #ifndef _WordList_h_
 #define _WordList_h_
 
-#include <Dictionary.h>
-#include <htString.h>
-
+#include "Dictionary.h"
+#include "htString.h"
+#include "Database.h"
+#include "WordRecord.h"
+#include "WordReference.h"
 
 class WordList
 {
@@ -30,14 +36,15 @@ public:
     //
     // Set some operating parameters
     //
-    void		WordTempFile(char *filename)	{tempfile = filename;}
     void		BadWordFile(char *filename);
     void		DocumentID(int id)		{docID = id;}
 
     //
     // Update/add a word
     //
-    void		Word(char *word, int location, int anchor_number, double weight);
+    void		Word(String word, unsigned int location, 
+			     unsigned int anchor_number, 
+			     unsigned long int flags);
 
     //
     // Mark a document as already scanned for words or mark it as disappeared
@@ -47,20 +54,52 @@ public:
     void		MarkModified();
 
     //
-    // Dump the words to a temporary words file
+    // Dump the words to the database
     //
     void		Flush();
 
     //
     // Check if the given word is valid
     //
-    int			IsValid(char *word)	{return valid_word(word);}
+    int			IsValid(String word)	{return valid_word(word);}
+
+
+    //
+    // Database access methods
+    //
+
+    int                 Open(char *filename);
+    int                 Read(char *filename);
+    int                 Close();
+
+    int                 Add(WordReference *wordRef);
+
+    // This returns a list of all the WordReference * for this word
+    List		*operator [] (String word);
+
+    int                 Exists(WordReference wordRef);
+    int                 Delete(WordReference wordRef);
+
+
+    //
+    // We will need to be able to iterate over the complete database.
+    //
+
+    // This returns a list of all the Words, as String *
+    List                *Words();
+    // This returns a list of all the Words, as WordReference *
+    List		*WordRefs();
 
 private:
     int			docID;
     String		tempfile;
     Dictionary		*words;
     Dictionary		badwords;
+
+    Database            *dbf;
+    int                 isopen;
+    int                 isread;
+
 
     int			valid_word(char *);
 };
