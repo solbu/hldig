b2ea996b5dde0f8b5b06232cbb3f7587fd781a55
grdetil <grdetil>
1999-09-01 20:56:32 +0000

    set up and use lookup table to speed up HtIsWordChar() and HtIsStrictWordChar().

--- htlib/HtWordType.cc
+++ htlib/HtWordType.cc
@@ -3,7 +3,7 @@
 // functions and other common functions without having to manage
 // the attributes or the exact attribute combination semantics.
 //
-// $Id: HtWordType.cc,v 1.1.2.1 1999/03/23 23:23:01 grdetil Exp $
+// $Id: HtWordType.cc,v 1.1.2.2 1999/09/01 20:56:32 grdetil Exp $
 //
 
 #include "HtWordType.h"
@@ -23,4 +23,17 @@ HtWordType::Initialize(Configuration &config)
   HtWordType::statics.extra_word_characters = extra_word_chars;
   HtWordType::statics.valid_punctuation = valid_punct;
   HtWordType::statics.other_chars_in_word = punct_and_extra;
+  HtWordType::statics.chrtypes[0] = 0;
+  for (int i = 1; i < 256; i++)
+  {
+    HtWordType::statics.chrtypes[i] = 0;
+    if (isalpha(i))
+	HtWordType::statics.chrtypes[i] |= HtWt_Alpha;
+    if (isdigit(i))
+	HtWordType::statics.chrtypes[i] |= HtWt_Digit;
+    if (strchr(extra_word_chars, i))
+	HtWordType::statics.chrtypes[i] |= HtWt_Extra;
+    if (strchr(valid_punct, i))
+	HtWordType::statics.chrtypes[i] |= HtWt_ValidPunct;
+  }
 }
