2b294384b66abc740fb652025d0ea2b153c9a147
ghutchis <ghutchis>
1999-09-05 18:25:39 +0000

    Implement a compare method for WordRefs to use in sorting. Uses String::nocase_compare()

--- htcommon/WordReference.h
+++ htcommon/WordReference.h
@@ -1,7 +1,7 @@
 //
 // WordReference.h
 //
-// $Id: WordReference.h,v 1.3 1999/07/19 01:49:09 ghutchis Exp $
+// $Id: WordReference.h,v 1.4 1999/09/05 18:25:39 ghutchis Exp $
 //
 //
 #ifndef _WordReference_h_
@@ -23,6 +23,8 @@ public:
 	long int		Flags;
 	int			Location;
 	int			Anchor;
+
+	int			compare(Object *to) {return Word.nocase_compare( ((WordReference *) to)->Word );}
 private:
 };
 
