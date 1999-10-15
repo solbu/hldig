38cb643f3e6fd9df04553bc853d7202d31bc34de
jtillman <jtillman>
1999-10-15 03:30:41 +0000

    smartened up the ResultList to avoid Display.cc having to access database directly

--- htsearch/ResultList.h
+++ htsearch/ResultList.h
@@ -10,7 +10,7 @@
 // or the GNU Public License version 2 or later
 // <http://www.gnu.org/copyleft/gpl.html>
 //
-// $Id: ResultList.h,v 1.4 1999/09/10 17:22:25 ghutchis Exp $
+// $Id: ResultList.h,v 1.5 1999/10/15 03:30:41 jtillman Exp $
 //
 
 #ifndef _ResultList_h_
@@ -19,24 +19,53 @@
 #include "Dictionary.h"
 #include "DocMatch.h"
 #include "HtVector.h"
+#include "DocumentDB.h"
 
 class ResultList : public Dictionary
 {
 public:
-    ResultList();
-    ~ResultList();
+	ResultList(const String& docFile, const String&, const String&);
+  ResultList();
+	~ResultList();
 
-    void		add(DocMatch *);
-    void		remove(int id);
-    DocMatch		*find(int id);
-    DocMatch		*find(char *id);
-    int			exists(int id);
+	void add(DocMatch *);
+  void remove(int id);
+  DocMatch *find(int id);
+  DocMatch *find(char *id);
+  int exists(int id);
 
-    HtVector		*elements();
+  HtVector *elements();
 
-    int			isIgnore;
+	/** Returns a reference to the data for the
+		document matching the id provided */
+  DocumentRef *getDocumentRef(int docID);
+  /** Retrieves the excerpt for the document into 
+memory */
+  void readExcerpt(DocumentRef &ref);
+
+	int			isIgnore;
+
+protected: // Protected attributes
+  /** Stores a reference to the database where 
+the data for each match can be retrieved */
+  DocumentDB docDB;
+  /** Indicates whether this is a "smart" ResultList
+or one that does not have a database
+connection */
+  int dbIsOpen;
 };
 
 #endif
+
+
+
+
+
+
+
+
+
+
+
 
 
