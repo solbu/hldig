7f50cf5cf6a8e786edf4d1c28f4f459e17d8b8cb
grdetil <grdetil>
1999-11-24 02:14:09 +0000

    htdig/ExternalParser.cc: added support for external converters as 	extension to external_parsers attribute. htcommon/defaults.cc: Updated external_parsers with new description 	and examples of external converters.

--- htdig/ExternalParser.cc
+++ htdig/ExternalParser.cc
@@ -7,10 +7,13 @@
 // The format is documented in http://www.htdig.org/attrs.html#external_parser
 //
 #if RELEASE
-static char RCSid[] = "$Id: ExternalParser.cc,v 1.9.2.2 1999/09/01 19:52:34 grdetil Exp $";
+static char RCSid[] = "$Id: ExternalParser.cc,v 1.9.2.3 1999/11/24 02:14:09 grdetil Exp $";
 #endif
 
 #include "ExternalParser.h"
+#include "HTML.h"
+#include "Plaintext.h"
+#include "PDF.h"
 #include "htdig.h"
 #include "htString.h"
 #include "QuotedStringList.h"
@@ -21,6 +24,7 @@ static char RCSid[] = "$Id: ExternalParser.cc,v 1.9.2.2 1999/09/01 19:52:34 grde
 #include "good_strtok.h"
 
 static Dictionary	*parsers = 0;
+static Dictionary	*toTypes = 0;
 extern String		configFile;
 
 //*****************************************************************************
@@ -88,13 +92,25 @@ ExternalParser::canParse(char *contentType)
     if (!parsers)
     {
 	parsers = new Dictionary();
+	toTypes = new Dictionary();
 	
 	QuotedStringList	qsl(config["external_parsers"], " \t");
+	String			from, to;
 	int			i;
+	int			sep;
 
 	for (i = 0; qsl[i]; i += 2)
 	{
-	    parsers->Add(qsl[i], new String(qsl[i + 1]));
+	    from = qsl[i];
+	    to = "";
+	    sep = from.indexOf("->");
+	    if (sep != -1)
+	    {
+		to = from.sub(sep+2).get();
+		from = from.sub(0, sep).get();
+	    }
+	    parsers->Add(from, new String(qsl[i + 1]));
+	    toTypes->Add(from, new String(to));
 	}
     }
     return parsers->Exists(contentType);
@@ -150,8 +166,45 @@ ExternalParser::parse(Retriever &retriever, URL &base)
     char	*token1, *token2, *token3;
     int		loc, hd;
     URL		url;
+    String	convertToType = ((String *)toTypes->Find(contentType))->get();
+    int		get_hdr = (mystrcasecmp(convertToType, "user-defined") == 0);
+    int		get_file = (convertToType.length() != 0);
+    String	newcontent;
     while (readLine(input, line))
     {
+	if (get_hdr)
+	{
+	    line.chop('\r');
+	    if (line.length() == 0)
+		get_hdr = FALSE;
+	    else if (mystrncasecmp(line, "content-type:", 13) == 0)
+	    {
+		token1 = line.get() + 13;
+		while (*token1 && isspace(*token1))
+		    token1++;
+		token1 = strtok(token1, "\n\t");
+		convertToType = token1;
+	    }
+	    continue;
+	}
+	if (get_file)
+	{
+	    if (newcontent.length() == 0 &&
+		!canParse(convertToType) &&
+		mystrncasecmp(convertToType, "text/", 5) != 0 &&
+		mystrncasecmp(convertToType, "application/pdf", 15) != 0)
+	    {
+		if (mystrcasecmp(convertToType, "user-defined") == 0)
+		    cerr << "External parser error: no Content-Type given\n";
+		else
+		    cerr << "External parser error: can't parse Content-Type \""
+			 << convertToType << "\"\n";
+		cerr << " URL: " << base.get() << "\n";
+		break;
+	    }
+	    newcontent << line << '\n';
+	    continue;
+	}
 	token1 = strtok(line, "\t");
 	if (token1 == NULL)
 	    token1 = "";
@@ -340,6 +393,50 @@ ExternalParser::parse(Retriever &retriever, URL &base)
     }
     pclose(input);
     unlink(path);
+
+    if (newcontent.length() > 0)
+    {
+	static HTML			*html = 0;
+	static Plaintext		*plaintext = 0;
+	static PDF			*pdf = 0;
+	Parsable			*parsable = 0;
+
+	contentType = convertToType;
+	if (canParse(contentType))
+	{
+	    currentParser = ((String *)parsers->Find(contentType))->get();
+	    parsable = this;
+	}
+	else if (mystrncasecmp(contentType, "text/html", 9) == 0)
+	{
+	    if (!html)
+		html = new HTML();
+	    parsable = html;
+	}
+	else if (mystrncasecmp(contentType, "text/plain", 10) == 0)
+	{
+	    if (!plaintext)
+		plaintext = new Plaintext();
+	    parsable = plaintext;
+	}
+	else if (mystrncasecmp(contentType, "application/pdf", 15) == 0)
+	{
+	    if (!pdf)
+		pdf = new PDF();
+	    parsable = pdf;
+	}
+	else
+	{
+	    if (!plaintext)
+		plaintext = new Plaintext();
+	    parsable = plaintext;
+	    if (debug)
+		cout << "External parser error: \"" << contentType <<
+			"\" not a recognized type.  Assuming text\n";
+	}
+	parsable->setContents(newcontent.get(), newcontent.length());
+	parsable->parse(retriever, base);
+    }
 }
 
 
