ff3a6e1b3b38277d92a051a783b0d311c059fb24
grdetil <grdetil>
2000-02-24 20:17:26 +0000

    * htlib/DB2_db.cc: Remove include of malloc.h

--- htlib/DB2_db.cc
+++ htlib/DB2_db.cc
@@ -1,43 +1,24 @@
 //
 // DB2_db.cc
 //
-// Implementation of DB2_db
+// DB2_db: Implements the Berkeley B-Tree database as a Database object
+//        (including duplicate values to allow duplicate word entries)
 //
-// $Log: DB2_db.cc,v $
-// Revision 1.4  1998/10/12 02:50:15  ghutchis
+// Part of the ht://Dig package   <http://www.htdig.org/>
+// Copyright (c) 1999 The ht://Dig Group
+// For copyright details, see the file COPYING in your distribution
+// or the GNU Public License version 2 or later 
+// <http://www.gnu.org/copyleft/gpl.html>
 //
-// Added fix suggested by Domotor Akos <dome@impulzus.sch.bme.hu> with (char
-// *)NULL cast.
+// $Id: DB2_db.cc,v 1.17.2.1 2000/02/24 20:17:27 grdetil Exp $
 //
-// Revision 1.3  1998/10/12 02:04:00  ghutchis
-//
-// Updated Makefiles and configure variables.
-//
-// Revision 1.2  1998/06/22 04:33:21  turtle
-// New Berkeley database stuff
-//
-// Revision 1.1  1998/06/21 23:20:06  turtle
-// patches by Esa and Jesse to add BerkeleyDB and Prefix searching
-//
-// Revision 1.1  1997/11/26  22:18:46  jesse
-// Initial revision
-//
-// Revision 1.1.1.1  1997/02/03 17:11:05  turtle
-// Initial CVS
-//
-//
-#if RELEASE
-static char RCSid[] = "$Id: DB2_db.cc,v 1.4 1998/10/12 02:50:15 ghutchis Exp $";
-#endif
 
 #include "DB2_db.h"
+
 #include <errno.h>
 #include <stdlib.h>
 #include <fstream.h>
-#include <malloc.h>
-
-// Where do I need this for? I don't know.
-int errno;
+#include <unistd.h>
 
 // Default cache size in kilobytes.
 // Maybe this should be an config option, just for easy testing and
@@ -51,6 +32,8 @@ int errno;
 DB2_db::DB2_db()
 {
     isOpen = 0;
+    _compare = 0;
+    _prefix = 0;
 }
 
 
@@ -59,80 +42,34 @@ DB2_db::DB2_db()
 //
 DB2_db::~DB2_db()
 {
-    if (isOpen)
-    {
-	Close();
-    }
+  Close();
 }
 
 
 //*****************************************************************************
-// int DB_db::OpenReadWrite(char *filename, int mode)
 //
 int
-DB2_db::OpenReadWrite(char *filename, int mode)
+DB2_db::Open(const char *filename, int flags, int mode)
 {
     //
     // Initialize the database environment.
     //
     dbenv = db_init((char *)NULL);
     memset(&dbinfo, 0, sizeof(dbinfo));
-//    dbinfo.db_cachesize = CACHE_SIZE_IN_KB * 1024;	// Cachesize: 64K.
-    dbinfo.db_pagesize = 1024;      			// Page size: 1K.
-
-    //
-    // Create the database.
-    //
-    if ((errno = db_open(filename, DB_BTREE, DB_CREATE, mode, dbenv,
-			 &dbinfo, &dbp)) == 0)
-    {
-        //
-	// Acquire a cursor for the database.
-	//
-        if ((seqrc = dbp->cursor(dbp, NULL, &dbcp)) != 0)
-	{
-            seqerr = seqrc;
-	    isOpen = 0;
-            Close();
-	    return NOTOK;
-        }
-	isOpen = 1;
-	return OK;
-    }
-    else
-    {
-	return NOTOK;
-    }
-}
-
-
-//*****************************************************************************
-// int DB2_db::OpenRead(char *filename)
-//
-int
-DB2_db::OpenRead(char *filename)
-{
-    //
-    // Initialize the database environment.
-    //
-    dbenv = db_init((char *)NULL);
-    memset(&dbinfo, 0, sizeof(dbinfo));
-//    dbinfo.db_cachesize = CACHE_SIZE_IN_KB * 1024;	// Cachesize: 64K.
-    dbinfo.db_pagesize = 1024;				// Page size: 1K.
+    dbinfo.bt_compare = _compare ? _compare : 0;
+    dbinfo.bt_prefix = _prefix ? _prefix : 0;
 
     //
     // Open the database.
     //
-    if ((errno = db_open(filename, DB_BTREE, DB_RDONLY, 0, dbenv,
-			 &dbinfo, &dbp)) == 0)
+    if((errno = db_open(filename, db_type, flags, mode, dbenv, &dbinfo, &dbp)) == 0)
     {
         //
 	// Acquire a cursor for the database.
 	//
-        if ((seqrc = dbp->cursor(dbp, NULL, &dbcp)) != 0)
+        if ((seqrc = dbp->cursor(dbp, NULL, &dbcp, 0)) != 0)
 	{
             seqerr = seqrc;
-	    isOpen = 0;
             Close();
 	    return NOTOK;
         }
@@ -152,7 +89,7 @@ DB2_db::OpenRead(char *filename)
 int
 DB2_db::Close()
 {
-    if (isOpen)
+    if(isOpen)
     {
 	//
 	// Close cursor, database and clean up environment
@@ -160,6 +97,8 @@ DB2_db::Close()
         (void)(dbcp->c_close)(dbcp);
 	(void)(dbp->close)(dbp, 0);
 	(void) db_appexit(dbenv);
+	free(dbenv);
+	dbenv = 0;
     }
     isOpen = 0;
     return OK;
@@ -167,73 +106,65 @@ DB2_db::Close()
 
 
 //*****************************************************************************
-// void DB2_db::Start_Get()
-//
-void
-DB2_db::Start_Get()
-{
-    DBT	nextkey;
-
-    //
-    // skey and nextkey are just dummies
-    //
-    memset(&nextkey, 0, sizeof(DBT));
-    memset(&skey, 0, sizeof(DBT));
-
-//    skey.data = "";
-//    skey.size = 0;
-//    skey.flags = 0;
-    if (isOpen && dbp)
-    {
-	//
-	// Set the cursor to the first position.
-	//
-        seqrc = dbcp->c_get(dbcp, &skey, &nextkey, DB_FIRST);
-	seqerr = seqrc;
-    }
-}
-
-
-//*****************************************************************************
-// char *DB2_db::Get_Next()
+// char *DB2_db::Get_Next(String &item, String &key)
 //
 char *
-DB2_db::Get_Next()
+DB2_db::Get_Next(String &item, String &key)
 {
-    //
-    // Looks like get Get_Next() and Get_Next_Seq() are pretty much the same...
-    //
-    DBT	nextkey;
-	
-    memset(&nextkey, 0, sizeof(DBT));
-
-    if (isOpen && !seqrc)
+  if (isOpen && !seqrc)
     {
-	lkey = 0;
-	lkey.append((char *)skey.data, skey.size);
-	// DON'T forget to set the flags to 0!
-	skey.flags = 0;
-        seqrc = dbcp->c_get(dbcp, &skey, &nextkey, DB_NEXT);
-	seqerr = seqrc;
-	return lkey.get();
+      //
+      // Return values
+      //
+      key = skey;
+      lkey = skey;
+      item = data;
+
+      //
+      // Search for the next record
+      //
+      DBT local_key;
+      DBT local_data;
+
+      memset(&local_key, 0, sizeof(DBT));
+      memset(&local_data, 0, sizeof(DBT));
+
+      local_key.data = skey.get();
+      local_key.size = skey.length();
+
+      seqrc = dbcp->c_get(dbcp, &local_key, &local_data, DB_NEXT);
+      seqerr = seqrc;
+
+      if(!seqrc) {
+	data = 0;
+	data.append((char*)local_data.data, (int)local_data.size);
+	skey = 0;
+	skey.append((char*)local_key.data, (int)local_key.size);
+      }
+
+      return lkey.get();
     }
-    else
-	return 0;
+  else
+    return 0;
 }
 
 //*****************************************************************************
 // void DB2_db::Start_Seq()
 //
 void
-DB2_db::Start_Seq(char *str)
+DB2_db::Start_Seq(const String& key)
 {
-    DBT	nextkey;
+    DBT local_key;
+    DBT local_data;
 
-    memset(&skey, 0, sizeof(DBT));
-    memset(&nextkey, 0, sizeof(DBT));
+    memset(&local_key, 0, sizeof(DBT));
+    memset(&local_data, 0, sizeof(DBT));
 
-    skey.data = str;
-    skey.size = strlen(str);
+    skey = key;
+
+    local_key.data = skey.get();
+    local_key.size = skey.length();
+
     if (isOpen && dbp)
     {
 	//
@@ -244,42 +175,57 @@ DB2_db::Start_Seq(char *str)
 	// anything. Setting to DB_SET_RANGE will still find the `first'
 	// word after boo* (which is book).
 	//
-        seqrc = dbcp->c_get(dbcp, &skey, &nextkey, DB_SET_RANGE);
+        seqrc = dbcp->c_get(dbcp, &local_key, &local_data, DB_SET_RANGE);
 	seqerr = seqrc;
+
+	if(!seqrc) {
+	  data = 0;
+	  data.append((char*)local_data.data, (int)local_data.size);
+	  skey = 0;
+	  skey.append((char*)local_key.data, (int)local_key.size);
+	}
     }
 }
 
-
 //*****************************************************************************
-// char *DB2_db::Get_Next_Seq()
+// void DB2_db::Start_Get()
 //
-char *
-DB2_db::Get_Next_Seq()
+void
+DB2_db::Start_Get()
 {
-    DBT	nextkey;
-	
-    memset(&nextkey, 0, sizeof(DBT));
+    DBT local_key;
+    DBT local_data;
 
-    if (isOpen && !seqrc)
+    memset(&local_key, 0, sizeof(DBT));
+    memset(&local_data, 0, sizeof(DBT));
+
+    if (isOpen && dbp)
     {
-	lkey = 0;
-	lkey.append((char *)skey.data, skey.size);
-	skey.flags = 0;
-        seqrc = dbcp->c_get(dbcp, &skey, &nextkey, DB_NEXT);
+	//
+	// Okay, get the first key. Use DB_SET_RANGE for finding partial
+	// keys also. If you set it to DB_SET, and the words book, books
+	// and bookstore do exists, it will find them if you specify
+	// book*. However if you specify boo* if will not find
+	// anything. Setting to DB_SET_RANGE will still find the `first'
+	// word after boo* (which is book).
+	//
+        seqrc = dbcp->c_get(dbcp, &local_key, &local_data, DB_FIRST);
 	seqerr = seqrc;
-	return lkey.get();
+
+	if(!seqrc) {
+	  data = 0;
+	  data.append((char*)local_data.data, (int)local_data.size);
+	  skey = 0;
+	  skey.append((char*)local_key.data, (int)local_key.size);
+	}
     }
-    else
-	return 0;
 }
 
-
-
 //*****************************************************************************
-// int DB2_db::Put(String &key, String &data)
+// int DB2_db::Put(const String &key, const String &data)
 //
 int
-DB2_db::Put(String &key, String &data)
+DB2_db::Put(const String &key, const String &data)
 {
     DBT	k, d;
 
@@ -289,10 +235,10 @@ DB2_db::Put(String &key, String &data)
     if (!isOpen)
 	return NOTOK;
 
-    k.data = key.get();
+    k.data = (char*)key.get();
     k.size = key.length();
 
-    d.data = data.get();
+    d.data = (char*)data.get();
     d.size = data.length();
 
     //
@@ -304,17 +250,20 @@ DB2_db::Put(String &key, String &data)
 
 
 //*****************************************************************************
-// int DB2_db::Get(String &key, String &data)
+// int DB2_db::Get(const String &key, String &data)
 //
 int
-DB2_db::Get(String &key, String &data)
+DB2_db::Get(const String &key, String &data)
 {
     DBT	k, d;
 
     memset(&k, 0, sizeof(DBT));
     memset(&d, 0, sizeof(DBT));
 
-    k.data = key.get();
+    //
+    // k arg of get should be const but is not. Harmless cast.
+    //
+    k.data = (char*)key.get();
     k.size = key.length();
 
     int rc = dbp->get(dbp, NULL, &k, &d, 0);
@@ -328,10 +277,10 @@ DB2_db::Get(String &key, String &data)
 
 
 //*****************************************************************************
-// int DB2_db::Exists(String &key)
+// int DB2_db::Exists(const String &key)
 //
 int
-DB2_db::Exists(String &key)
+DB2_db::Exists(const String &key)
 {
     String data;
 
@@ -343,10 +292,10 @@ DB2_db::Exists(String &key)
 
 
 //*****************************************************************************
-// int DB2_db::Delete(String &key)
+// int DB2_db::Delete(const String &key)
 //
 int
-DB2_db::Delete(String &key)
+DB2_db::Delete(const String &key)
 {
     DBT	k;
 
@@ -355,7 +304,7 @@ DB2_db::Delete(String &key)
     if (!isOpen)
 	return 0;
 
-    k.data = key.get();
+    k.data = (char*)key.get();
     k.size = key.length();
 
     return (dbp->del)(dbp, NULL, &k, 0);
@@ -366,7 +315,7 @@ DB2_db::Delete(String &key)
 // DB2_db *DB2_db::getDatabaseInstance()
 //
 DB2_db *
-DB2_db::getDatabaseInstance()
+DB2_db::getDatabaseInstance(DBTYPE)
 {
     return new DB2_db();
 }
