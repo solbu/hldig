1871d2282e4c135a04f7ff4e8a427cdbcc998b8f
ghutchis <ghutchis>
2000-03-20 19:13:55 +0000

    New program. Simply spits up the total number of documents, words and unique words in the databases.

---
+++ httools/htstat.cc
@@ -0,0 +1,164 @@
+//
+// htstat.cc
+//
+// htstat: A utility to give statistics on the contents of the word and doc DB.
+//
+// Part of the ht://Dig package   <http://www.htdig.org/>
+// Copyright (c) 1999-2000 The ht://Dig Group
+// For copyright details, see the file COPYING in your distribution
+// or the GNU Public License version 2 or later
+// <http://www.gnu.org/copyleft/gpl.html>
+//
+// $Id: htstat.cc,v 1.1.2.1 2000/03/20 19:13:55 ghutchis Exp $
+//
+
+#include "HtURLCodec.h"
+#include "HtWordList.h"
+#include "HtConfiguration.h"
+#include "DocumentDB.h"
+#include "defaults.h"
+
+#include <errno.h>
+#include <unistd.h>
+
+// If we have this, we probably want it.
+#ifdef HAVE_GETOPT_H
+#include <getopt.h>
+#endif
+
+int		verbose = 0;
+
+void usage();
+void reportError(char *msg);
+
+//*****************************************************************************
+// int main(int ac, char **av)
+//
+int main(int ac, char **av)
+{
+    int			alt_work_area = 0;
+    String		configfile = DEFAULT_CONFIG_FILE;
+    int			c;
+    extern char		*optarg;
+
+    while ((c = getopt(ac, av, "vc:a")) != -1)
+    {
+	switch (c)
+	{
+	    case 'c':
+		configfile = optarg;
+		break;
+	    case 'v':
+		verbose++;
+		break;
+	    case 'a':
+		alt_work_area++;
+		break;
+	    case '?':
+		usage();
+		break;
+	}
+    }
+
+    config.Defaults(&defaults[0]);
+
+    if (access((char*)configfile, R_OK) < 0)
+    {
+	reportError(form("Unable to find configuration file '%s'",
+			 configfile.get()));
+    }
+	
+    config.Read(configfile);
+
+    //
+    // Check url_part_aliases and common_url_parts for
+    // errors.
+    String url_part_errors = HtURLCodec::instance()->ErrMsg();
+
+    if (url_part_errors.length() != 0)
+      reportError(form("Invalid url_part_aliases or common_url_parts: %s",
+                       url_part_errors.get()));
+
+
+    // We may need these through the methods we call
+    if (alt_work_area != 0)
+    {
+	String	configValue;
+
+	configValue = config["word_db"];
+	if (configValue.length() != 0)
+	{
+	    configValue << ".work";
+	    config.Add("word_db", configValue);
+	}
+
+	configValue = config["doc_db"];
+	if (configValue.length() != 0)
+	{
+	    configValue << ".work";
+	    config.Add("doc_db", configValue);
+	}
+
+	configValue = config["doc_index"];
+	if (configValue.length() != 0)
+	{
+	    configValue << ".work";
+	    config.Add("doc_index", configValue);
+	}
+
+	configValue = config["doc_excerpt"];
+	if (configValue.length() != 0)
+	{
+	    configValue << ".work";
+	    config.Add("doc_excerpt", configValue);
+	}
+    }
+
+    DocumentDB docs;
+    if (docs.Read(config["doc_db"], config["doc_index"], 
+		  config["doc_excerpt"]) == OK)
+      {
+	cout << "htstat: Total documents: " << docs.URLs()->Count() << endl;
+      }
+
+    HtWordList words(config);
+    if(words.Open(config["word_db"], O_RDONLY) == OK)
+      {
+	cout << "htstat: Total words: " << words.WordRefs()->Count() << endl;
+	cout << "htstat: Total unique words: " << words.Words()->Count() << endl;
+      }
+
+}
+
+
+//*****************************************************************************
+// void usage()
+//   Display program usage information
+//
+void usage()
+{
+    cout << "usage: htstat [-v][-a][-c configfile]\n";
+    cout << "This program is part of ht://Dig " << VERSION << "\n\n";
+    cout << "Options:\n";
+    cout << "\t-v\tVerbose mode.  This increases the verbosity of the\n";
+    cout << "\t\tprogram.  Using more than 2 is probably only useful\n";
+    cout << "\t\tfor debugging purposes.  The default verbose mode\n";
+    cout << "\t\tgives a progress on what it is doing and where it is.\n\n";
+    cout << "\t-a\tUse alternate work files.\n";
+    cout << "\t\tTells htstat to append .work to the database files \n";
+    cout << "\t\tallowing it to operate on a second set of databases.\n";
+    cout << "\t-c configfile\n";
+    cout << "\t\tUse the specified configuration file instead on the\n";
+    cout << "\t\tdefault.\n\n";
+    exit(0);
+}
+
+
+//*****************************************************************************
+// Report an error and die
+//
+void reportError(char *msg)
+{
+    cout << "htstat: " << msg << "\n\n";
+    exit(1);
+}
