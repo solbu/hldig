3a4f304a407a2e18ff06940386afec9ffbad1cf2
andy5995 <andy400-dev@yahoo.com>
2017-11-26 18:52:40 -0600

    include dir:indent

--- include/htconfig.h
+++ include/htconfig.h
@@ -11,18 +11,17 @@
 #include <config.h>
 
 #if HAVE_STDBOOL_H
-# include <stdbool.h>
+#include <stdbool.h>
 #else
-# if ! HAVE__BOOL
-#  ifdef __cplusplus
-  typedef bool _Bool;
-#  else
-  typedef unsigned char _Bool;
-#  endif
-# endif
-# define bool _Bool
-# define false 0
-# define true 1
-# define __bool_true_false_are_defined 1
+#if ! HAVE__BOOL
+#ifdef __cplusplus
+typedef bool _Bool;
+#else
+typedef unsigned char _Bool;
 #endif
-
+#endif
+#define bool _Bool
+#define false 0
+#define true 1
+#define __bool_true_false_are_defined 1
+#endif
