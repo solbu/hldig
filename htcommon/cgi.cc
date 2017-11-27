76c1651ed7e125cc064f4aeedb35ad0749e86b18
andy5995 <andy400-dev@yahoo.com>
2017-11-27 00:43:30 -0600

    htcommon/cgi:use const to declare exists()

--- htcommon/cgi.cc
+++ htcommon/cgi.cc
@@ -6,7 +6,7 @@
 // Part of the ht://Dig package   <http://www.htdig.org/>
 // Copyright (c) 1995-2004 The ht://Dig Group
 // For copyright details, see the file COPYING in your distribution
-// or the GNU Library General Public License (LGPL) version 2 or later 
+// or the GNU Library General Public License (LGPL) version 2 or later
 // <http://www.gnu.org/copyleft/lgpl.html>
 //
 // $Id: cgi.cc,v 1.9 2004/05/28 13:15:12 lha Exp $
@@ -189,7 +189,7 @@ cgi::get (const char *name)
 // int cgi::exists(char *name)
 //
 int
-cgi::exists (char *name)
+cgi::exists (const char *name)
 {
   return pairs->Exists (name);
 }
--- htcommon/cgi.h
+++ htcommon/cgi.h
@@ -6,7 +6,7 @@
 // Part of the ht://Dig package   <http://www.htdig.org/>
 // Copyright (c) 1995-2004 The ht://Dig Group
 // For copyright details, see the file COPYING in your distribution
-// or the GNU Library General Public License (LGPL) version 2 or later 
+// or the GNU Library General Public License (LGPL) version 2 or later
 // <http://www.gnu.org/copyleft/lgpl.html>
 //
 // $Id: cgi.h,v 1.4 2004/05/28 13:15:12 lha Exp $
@@ -26,7 +26,7 @@ public:
 
   const char *operator [] (const char *);
   const char *get (const char *);
-  int exists (char *);
+  int exists (const char *);
   char *path ();
 
 private:
