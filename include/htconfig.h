dc6081364f0e544f2aa1c2e375d4454660ebb7f0
andy5995 <andy400-dev@yahoo.com>
2018-01-01 10:38:51 -0600

    try libintl.h instead of gettext.h

--- include/htconfig.h
+++ include/htconfig.h
@@ -10,7 +10,8 @@
 
 #include <config.h>
 
-#include "gettext.h"
+// #include "gettext.h"
+#include <libintl.h>
 #define _(String) gettext (String)
 #define gettext_noop(String) String
 #define N_(String) gettext_noop (String)
